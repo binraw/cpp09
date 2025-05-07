6ad5b1606b6b61a001b31c3c2f517b0b2511d875
Robin Truvelot <rtruvelo@z2r11p7.42lyon.fr>
2025-05-07 10:28:14 +0200

    valid algo for deque 100%

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -15,7 +15,15 @@ PmergeMe::PmergeMe(int argc, char **argv)
     createDoublon(argc, argv);
     _simpleTest = createSimpleContainer(_containerDequePair);
     recursiveSort(_simpleTest);
-    
+    recursiveInsertWithContainer(_simpleTest, 1);
+
+    if (_containerDequeRest.size() == 1)
+    {
+        std::deque<int>::iterator it = _containerDequeRest.begin();
+        std::deque<int>::iterator posValid = std::lower_bound(_simpleTest.begin(), _simpleTest.end(), *it);
+        _simpleTest.insert(posValid, *it);
+
+    }
     std::cout << "Finale list elements : " << std::endl;
     iter(_simpleTest, printElement<int>);
     // createlargestContainer();
@@ -89,93 +97,7 @@ void PmergeMe::controlValue(char *value)
         throw ErrorNull(); // no value
 }
 
-// void PmergeMe::createDoublon(int argc, char **argv)
-// {
-//     if ((argc % 2) == 0) // si impair args
-//     {
-//         int i = 1;
-//         controlValue(argv[i]);
-//         _containerDequeLong.push_back(std::atoi(argv[i]));
-//         for (i = 2; i < argc; i += 2) 
-//         {
-//             if (i + 1 < argc)
-//             {
-//                 controlValue(argv[i]);
-//                 controlValue(argv[i + 1]);
-//                 int first = std::atoi(argv[i]);
-//                 int second = std::atoi(argv[i + 1]);
-//                 _containerDequePair.push_back(std::make_pair(first, second));
-//             }
-//         }
-//     }
-//     else 
-//     {
-//         for (int i = 1; i < argc; i += 2) 
-//         {
-//             if (i + 1 < argc)
-//             {
-//                 controlValue(argv[i]);
-//                 controlValue(argv[i + 1]);
-//                 int first = std::atoi(argv[i]);
-//                 int second = std::atoi(argv[i + 1]);
-//                 _containerDequePair.push_back(std::make_pair(first, second));
-//             }
-//         }
-//     }
 
-//     for (std::deque<std::pair<int, int>>::iterator it = _containerDequePair.begin(); it != _containerDequePair.end(); it++)
-//     {
-//         if (::min(it->first, it->second) == it->second)
-//             ::swap(it->first, it->second);
-//     }
-// }
-
-
-// void PmergeMe::createlargestContainer()
-// {
-//     for (std::deque<std::pair<int, int>>::iterator it = _containerDequePair.begin(); it != _containerDequePair.end(); it++)
-//         _containerDequeLong.push_back(it->second);
-//      recursiveSort(_containerDequeLong);
-// }
-
-
-// void PmergeMe::recursiveSort(std::deque<int>& arr) 
-// {
-//     if (arr.size() <= 1) 
-//         return;
-
-
-//     size_t mid = arr.size() / 2;
-//     std::deque<int> left(arr.begin(), arr.begin() + mid);
-//     std::deque<int> right(arr.begin() + mid, arr.end());
-
-
-//     recursiveSort(left);
-//     recursiveSort(right);
-
-
-//     size_t i = 0, j = 0, k = 0;
-//     while (i < left.size() && j < right.size()) 
-//     {
-//         if (left[i] <= right[j]) 
-//         {
-//             arr[k++] = left[i++];
-//         } 
-//         else 
-//         {
-//             arr[k++] = right[j++];
-//         }
-//     }
-
-//     while (i < left.size()) 
-//     {
-//         arr[k++] = left[i++];
-//     }
-//     while (j < right.size()) 
-//     {
-//         arr[k++] = right[j++];
-//     }
-// }
 
 void PmergeMe::reorderContainerPair()
 {
@@ -229,18 +151,6 @@ const char* PmergeMe::ErrorRange::what() const  throw()
 }
 
 
-/* CHANGEMENT DE PLAN :
-Calcul du Nombre de Paires
-Gestion des Paires Impaires
-Détermination des Itérateurs
-Échange des Paires
-Appel Recursif
-Gestion des Éléments Restants
-Insertion des Éléments Restants
-Remplacement des Valeurs dans le Conteneur Original
-
-*/
-
 
 
 void PmergeMe::createDoublon(int argc, char **argv)
@@ -288,134 +198,7 @@ void PmergeMe::createDoublon(int argc, char **argv)
     }
 }
 
-// void PmergeMe::recursiveSort(std::deque<std::pair<int, int>>& arr)
-// {
-//     if (arr.empty()) 
-//         return;
-//     int actualLevels = _levels;
-//     int groupCount = arr.size() / actualLevels; // ici pour verifier qu'il y a toujours des comparaisons possible vu qu'on regroupe de plus en plus
-//     if (groupCount < 2)
-//         return;
-//     // bool groupOdd = false;
-//     // if ((groupCount % 2) != 0)
-//     //     groupOdd = true;
-//     std::deque<std::pair<int, int>>::iterator start = arr.begin();
-//     std::deque<std::pair<int, int>>::iterator end = arr.end(); // obliger de initialiser avant advance
-//     std::deque<std::pair<int, int>>::iterator lastGroup = start;
-//     // std::advance(end, _levels * groupCount);
-//     if (groupCount > 0) 
-//     {
-//         std::advance(lastGroup, -(groupCount)); 
-//         // if (lastGroup < start) {
-//         //     lastGroup = start; 
-//         // }
-//     }
-//     for (std::deque<std::pair<int, int>>::iterator it = arr.begin(); it != lastGroup; std::advance(it, actualLevels))
-//     { // ici je dois determiner la comparaison 
-//         /* jai mis actualLevels a 1 de base car je procede deja avec des paires
-//         */
-//         std::deque<std::pair<int, int>>::iterator maxGroupActual = it;
-//         std::deque<std::pair<int, int>>::iterator maxGroupeNextIt = it + actualLevels;
-//         if (maxGroupeNextIt < end)
-//         {
-//             std::cout << "echange de groupe" << std::endl;
-//             if (::min(maxGroupActual->second, maxGroupeNextIt->second) == maxGroupeNextIt->second)
-//             {
-//                 std::cout << "changement avec : " << maxGroupActual->second <<  " et : " << maxGroupeNextIt->second << std::endl; 
-//                 ::swap(*maxGroupActual, *maxGroupeNextIt); // la je crois que je vais bien swap mais a verifier
-//             }
-//         }
 
-//     }
-//     _levels += 1; // pour donner au prochain une pair de plus dans son groupe
-//     recursiveSort(arr);
-//      /* La je doit creer le main et le rest container 
-//         et je dois separer les paires en gros je dois bien passer en monde deque simple
-//         car apres je vais inserer par rapport a l'index de chaque nombre 
-
-//         enfaite dans les premiere insertion on doit seulement regarder le plus grand
-//         donc possible de continuer avec un deque pair je pense 
-//         et quand on arrive sur la derniere insertion de nombre a nombre 
-//         alors la creer des container 'simple'
-//      */
-//    // std::vector<std::deque<std::pair<int, int>>::iterator> mainContainer; // je mets des iterator pour que ce soit plus flexible dans un vector pour insert
-//     //std::vector<std::deque<std::pair<int, int>>::iterator> restContainer;
-
-//     //std::deque<std::pair<int, int>>::iterator beginInsert = arr.begin();
-//     //std::advance(beginInsert, actualLevels); // je fais avancer a la premiere pair a mettre
-//     // mais j'ai l'impression de devoir mettre tout se qu'il y a devant aussi vu qu'on sait deja que les grands sont trier
-//     // je mets direcctement les deux pairs au debut
-//     // mainContainer.insert(mainContainer.end(), beginInsert);
-
-// }
-
-
-// void PmergeMe::recursiveSort(std::deque<int>& arr)
-// {
-//     std::cout << "rentre bien dans la recursive" << std::endl;
-//     if (arr.empty()) 
-//         return;
-//     int actualLevels = _levels;
-//     int groupCount = arr.size() / actualLevels; // ici pour verifier qu'il y a toujours des comparaisons possible vu qu'on regroupe de plus en plus
-//     if (groupCount < 2)
-//         return;
-//     bool groupOdd = false;
-//     if ((groupCount % 2) != 0)
-//         groupOdd = true;
-//     std::deque<int>::iterator start = arr.begin();
-//     std::deque<int>::iterator end = arr.end(); // obliger de initialiser avant advance
-//     std::deque<int>::iterator lastGroup = start;
-//     std::advance(end, actualLevels * groupCount);
-
-//     std::advance(lastGroup, -((arr.size() / actualLevels) % 2));
-//         // std::advance(lastGroup, -(groupOdd * groupCount)); 
-//         // if (lastGroup < start) {
-//         //     lastGroup = start; 
-//         // }
-
-//     int jump = 2 * actualLevels;
-//     for (std::deque<int>::iterator it = arr.begin(); it != lastGroup; std::advance(it, jump))
-//     { // ici je dois determiner la comparaison 
-//         /* jai mis actualLevels a 1 de base car je procede deja avec des paires
-//         */
-//        std::deque<int>::iterator maxGroupActual = it + (actualLevels - 1);
-//        std::deque<int>::iterator maxGroupeNextIt = it + (actualLevels * 2 - 1);
-
-//         if (maxGroupeNextIt < end)
-//         {
-//             if (*maxGroupeNextIt < *maxGroupActual)
-//             {
-//                 std::cout << "echange : " << *maxGroupActual << " avec " << *maxGroupeNextIt << std::endl;
-//                  ::swap(*maxGroupActual, *maxGroupeNextIt);
-//             }
-//         }
-
-
-//     }
-//     _levels *= 2; // pour donner au prochain une pair de plus dans son groupe
-//     recursiveSort(arr);
-//      /* La je doit creer le main et le rest container 
-//         et je dois separer les paires en gros je dois bien passer en monde deque simple
-//         car apres je vais inserer par rapport a l'index de chaque nombre 
-
-//         enfaite dans les premiere insertion on doit seulement regarder le plus grand
-//         donc possible de continuer avec un deque pair je pense 
-//         et quand on arrive sur la derniere insertion de nombre a nombre 
-//         alors la creer des container 'simple'
-//      */
-//    // std::vector<std::deque<std::pair<int, int>>::iterator> mainContainer; // je mets des iterator pour que ce soit plus flexible dans un vector pour insert
-//     //std::vector<std::deque<std::pair<int, int>>::iterator> restContainer;
-
-//     //std::deque<std::pair<int, int>>::iterator beginInsert = arr.begin();
-//     //std::advance(beginInsert, actualLevels); // je fais avancer a la premiere pair a mettre
-//     // mais j'ai l'impression de devoir mettre tout se qu'il y a devant aussi vu qu'on sait deja que les grands sont trier
-//     // je mets direcctement les deux pairs au debut
-//     // mainContainer.insert(mainContainer.end(), beginInsert);
-
-// }
-
-
-/* */
 void swap_pair(std::deque<int>::iterator it, int pair_level, std::deque<int> &container)
 {
     std::deque<int>::iterator start = nextIt(it, -pair_level + 1);
@@ -662,29 +445,14 @@ void PmergeMe::recursiveInsertWithContainer(std::deque<int>& arr, int actualLeve
         mainPart.push_back(*maxGroupB);  
         mainPart.push_back(*maxGroupA);
 
-// la il me manque la derniere partie de maxA 
-    // std::cout << "Main Part du lvl : " << actualLevels << std::endl;
-    // iterContainerOfContainer(mainPart, printElement<int>);
-    // std::cout << "fin main Part." << std::endl;
-
-    if (pair_units_nbr > 2) // ici du coup le reste des valeurs a trier 
+    if (pair_units_nbr > 2)
     {
         for (std::deque<std::deque<int>>::iterator it = maxGroupA + 1; it != allContainer.end(); it++)
         {
             pendingPart.push_back(*it);
         }
-        // std::cout << "Valeur de pendingPart level : " << actualLevels << std::endl;
-        // iterContainerOfContainer(pendingPart, printElement<int>);
-        // std::cout << "FIN PENDINGPART " << std::endl;
     }
-
-
-
-    // std::cout << "VALUE SEULEMENT DES MAX MAIN: " << std::endl;
-    // iter(maxValueMain, printElement<int>);
-    // std::cout << "FIN DES MAX DE MAIN" << std::endl;
-        
-        
+   
     if (pendingPart.size() != 0)
     {
         std::deque<std::deque<int>>::iterator startPending = pendingPart.begin();
